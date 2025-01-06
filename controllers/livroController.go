06f5a78dcbc736e2e138db6d1cecb4c8204a3c95
Herick2D <contato.herickmoreira@gmail.com>
2025-01-06 17:20:09 -0300

    FIX: resposta update e delete, para se tornar mais coerente

--- controllers/livroController.go
+++ controllers/livroController.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"gorm.io/gorm"
 	"livroApi/config"
 	"livroApi/models"
 	"net/http"
@@ -35,25 +36,50 @@ func GetLivro(c *gin.Context) {
 }
 
 func UpdateLivro(c *gin.Context) {
-	id := c.Param("id")
 	var livro models.Livro
+	id := c.Param("id")
+
 	if err := config.DB.First(&livro, id).Error; err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Livro não encontrado"})
 		return
 	}
-	if err := c.ShouldBindJSON(&livro); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+
+	var input models.Livro
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Dados inválidos ou incompletos: " + err.Error()})
 		return
 	}
-	config.DB.Save(&livro)
+
+	livro.Titulo = input.Titulo
+	livro.Categoria = input.Categoria
+	livro.Autor = input.Autor
+	livro.Sinopse = input.Sinopse
+
+	if result := config.DB.Save(&livro); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Falha ao atualizar o livro: " + result.Error.Error()})
+		return
+	}
+
 	c.JSON(http.StatusOK, livro)
 }
 
 func DeleteLivro(c *gin.Context) {
 	id := c.Param("id")
-	if err := config.DB.Delete(&models.Livro{}, id).Error; err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Livro não encontrado"})
+	var livro models.Livro
+
+	if err := config.DB.First(&livro, id).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Livro não encontrado"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao buscar o livro: " + err.Error()})
+		}
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"message": "Livro excluído com sucesso"})
+
+	if err := config.DB.Delete(&livro).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao excluir o livro: " + err.Error()})
+		return
+	}
+
+	c.Status(http.StatusNoContent)
 }
