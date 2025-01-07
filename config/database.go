ef28e7ebb43bf63b710873d9bd327780528fa1cf
Herick2D <contato.herickmoreira@gmail.com>
2025-01-06 23:06:25 -0300

    FEAT: adição das variáveis de ambiente docker-compose

--- config/database.go
+++ config/database.go
@@ -1,16 +1,32 @@
 package config
 
 import (
+	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"livroApi/models"
 	"log"
+	"os"
 )
 
 var DB *gorm.DB
 
 func ConexaoDB() {
-	dsn := "host=postgres dbname=livroapidb user=herick password=123321 port=5432 sslmode=disable TimeZone=America/Sao_Paulo"
+
+	data := struct {
+		Host     string
+		User     string
+		Password string
+		DbName   string
+	}{
+		Host:     os.Getenv("HOST"),
+		User:     os.Getenv("USER"),
+		Password: os.Getenv("PASSWORD"),
+		DbName:   os.Getenv("DBNAME"),
+	}
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=America/Sao_Paulo",
+		data.Host, data.User, data.Password, data.DbName)
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
