7ad45c735d901daea98f9f61e6ae925f4f6bda30
Herick2D <contato.herickmoreira@gmail.com>
2025-05-01 11:21:30 -0300

    CHORE: CORS policy

--- main.go
+++ main.go
@@ -4,14 +4,26 @@ import (
 	"livroApi/config"
 	"livroApi/models"
 	"livroApi/routes"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
+
 	config.ConexaoDB()
 
 	config.DB.AutoMigrate(&models.Livro{})
 
 	router := routes.SetupRouter()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
 	router.Run(":8080")
 }
